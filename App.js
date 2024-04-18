d90ca72c6d3881c7136a46162fa9ade8205ecafb
Brigham Baker <brigham.baker@lethbridgecollege.ca>
2024-04-18 13:13:10 -0600

    added preventAutoHideAsync to ensure splash screen shows in build.

--- App.js
+++ App.js
@@ -3,9 +3,14 @@
 import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View, Platform, StyleSheet } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 import { initDB } from './database/db';
 import HomeScreen from './screens/HomeScreen';
 import styles from './styles/master-style-sheet';
+
+SplashScreen.preventAutoHideAsync();
+
+setTimeout(SplashScreen.hideAsync, 3000);
 
 export default function App() {
     useEffect(() => {
